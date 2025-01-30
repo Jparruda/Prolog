6e98b0e6d3a826cd83795ebb12372d7388af0845
João Pedro Souza Arruda <125840345+Jparruda@users.noreply.github.com>
2025-01-29 22:28:10 -0300

    Create aula1.pl

---
+++ aula1.pl
@@ -0,0 +1,28 @@
+% Definição de homens
+homem(ivo).
+homem(rai).
+homem(noe).
+homem(gil).
+homem(ary).
+
+% Definição de mulheres
+mulher(ana).
+mulher(eva).
+mulher(bia).
+mulher(clo).
+mulher(lia).
+mulher(gal).
+
+% Definição de pais
+pai(ivo, eva).
+pai(rai, noe).
+pai(gil, clo).
+pai(gil, ary).
+pai(ary, gal).
+
+% Definição de mães
+mãe(ana, eva).
+mãe(eva, noe).
+mãe(bia, clo).
+mãe(bia, ary).
+mãe(lia, gal).
